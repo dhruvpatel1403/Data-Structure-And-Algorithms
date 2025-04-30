39a9acae531206dd3596f16f03e79ebeb95d7f7f
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-04-30 15:30:07 -0300

    Array : Binary Search

--- Arrays/Code.java
+++ Arrays/Code.java
@@ -11,9 +11,28 @@ public class Code{
         }
         search(arr, n+1, key);
     }
+    public static void binarySearch(int arr[],int target){
+        int si = 0;
+        int ei = arr.length-1;
+        
+        while(si<=ei){
+            int mid= si + (ei-si)/2;
+            if(arr[mid]==target){
+                System.out.println("ELement Found at : "+mid);
+                return;
+            }
+            else if(arr[mid]>target){
+                ei = mid-1;
+            }
+            else{
+                si = mid+1;
+            }
+        }
+        System.out.println("Element Not Found");
+    }
     public static void main(String args[]){
-        int arr[] = {1,9,3,5,8};
-        search(arr, 0, 0);
-
+        int arr[] = {1,2,3,4,5};
+        // search(arr, 0, 0);
+        binarySearch(arr, 2);
     }
 }
