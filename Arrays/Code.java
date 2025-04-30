ca234f710ed989c43cd0e806d8c81ec2521f6ae8
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-04-30 19:03:54 -0300

    Array : Trapping Water

--- Arrays/Code.java
+++ Arrays/Code.java
@@ -107,9 +107,31 @@ public class Code{
         }
         System.out.println(max_sum);
     }
-    
+    public static void trappindWater(int arr[]){
+        // leftmax
+
+        int n=arr.length;
+        int left[] = new int[n];
+        left[0] = arr[0];
+        for(int i=1;i<n;i++){
+            left[i] = Math.max(arr[i],left[i-1]);
+        }
+        // rightmax
+        int right[] = new int[n];
+        right[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i] = Math.max(arr[i],right[i+1]);
+        }
+        // waterlevel
+        // max trapped water
+        int trappedWater = 0;
+        for(int i=0;i<n;i++){
+            trappedWater += (Math.min(left[i],right[i])) - arr[i];
+        }
+        System.out.println(trappedWater);
+    }
     public static void main(String args[]){
-        int arr[] = {-1,2,-3,4,5};
+        int arr[] = {4,2,0,6,3,2,5};
         // search(arr, 0, 0);
         // binarySearch(arr, 2);
         // reverseArray(arr);
@@ -119,5 +141,6 @@ public class Code{
         subArraySum(arr);
         maxSumPrefix(arr);
         kdanesAlgo(arr);
+        trappindWater(arr);
     }
 }
