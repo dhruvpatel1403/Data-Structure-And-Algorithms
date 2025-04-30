649678fd5273a30507f95b6993a39027e15230f7
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-04-30 15:36:20 -0300

    Array : Pairs in array

--- Arrays/Code.java
+++ Arrays/Code.java
@@ -47,11 +47,20 @@ public class Code{
         }
         System.out.println();
     }
+    public static void pairsInArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print(" ( "+arr[i]+" , "+ arr[j]+" )");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String args[]){
         int arr[] = {1,2,3,4,5};
         // search(arr, 0, 0);
         // binarySearch(arr, 2);
-        reverseArray(arr);
-        printArray(arr);
+        // reverseArray(arr);
+        // printArray(arr);
+        pairsInArray(arr);
     }
 }
