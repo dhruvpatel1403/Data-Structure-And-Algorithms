e7bb8e519b39451960bef95418aaa2ad9ce192cc
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-01 09:16:17 -0300

    2D Array : Find transpose

--- Arrays2D/Assignment.java
+++ Arrays2D/Assignment.java
@@ -19,9 +19,28 @@ public class Assignment {
         }
         System.out.println("Sum is : "+sum);
     }
+    public static void findTranspose(int arr[][]){
+        int transpose[][] =new int[arr[0].length][arr.length];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                transpose[j][i] = arr[i][j];
+            }
+        }
+        print2D(transpose);
+    }
+    public static void print2D(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int arr[][] = {{1,7,9},{8,8,7}};
         // count7(arr);
-        sum2ndRow(arr);
+        // sum2ndRow(arr);
+        print2D(arr);
+        findTranspose(arr);
     }
 }
