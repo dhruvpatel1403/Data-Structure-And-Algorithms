9a859114592b7b52f1f2ed43418ae8b740e863f1
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-04-30 21:05:46 -0300

    Sorting : Selection Sort

--- sorting/Assignment.java
+++ sorting/Assignment.java
@@ -17,7 +17,19 @@ public class Assignment {
             }
         }
     }
-   
+    public static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int min = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[min]>arr[j]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
     public static void printArr(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
