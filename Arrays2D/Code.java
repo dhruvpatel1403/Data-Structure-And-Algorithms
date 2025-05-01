995b679e8595bd7f02b3841e2991964a5e46d1ea
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-01 08:49:36 -0300

    2D Array : Diagonal Sum

--- Arrays2D/Code.java
+++ Arrays2D/Code.java
@@ -28,9 +28,24 @@ public class Code {
             end_col--;
             end_row--;
         }
+        System.out.println();
+    }
+    public static void diagonal_sum(int arr[][]){
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i][i];
+            if(arr.length-i-1 != i){
+                sum+=arr[i][arr.length-i-1];
+            }
+        }
+        System.out.println("Diagonal sum is : "+sum);
     }
     public static void main(String[] args) {
-        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int arr[][] = {{1,2,3},{5,6,7},{9,10,11}};
         spiral(arr);
+        diagonal_sum(arr);
     }
 }
