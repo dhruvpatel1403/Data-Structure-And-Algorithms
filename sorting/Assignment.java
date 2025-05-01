5a4d3a693173d35841453a5dd826cf5c052c9f8a
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-04-30 21:05:14 -0300

    Sorting : bubble Sort

---
+++ sorting/Assignment.java
@@ -0,0 +1,33 @@
+package sorting;
+
+public class Assignment {
+    public static void bubbleSort(int arr[]){
+        Boolean swap=false;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swap = true;
+                }
+            }
+            if(!swap){
+                break;
+            }
+        }
+    }
+   
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]= {12,12,3,2,3,1};
+        // bubbleSort(arr);
+        selectionSort(arr);
+        printArr(arr);
+    }
+}
