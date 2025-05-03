6fd246aa745a777ce1ecf214b9eaac3fa6866236
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-03 09:50:32 -0300

    Bit : clear bit in range

--- bit/BitsCode.java
+++ bit/BitsCode.java
@@ -16,7 +16,17 @@ public class BitsCode {
     public static void clearIthBit(int number,int i){
         System.out.println(number & (~(1<<i)));
     }
+    public static void clearLastIthBit(int number,int i){
+        // 11110000 --> ~1 << 4
+        System.out.println(number & ((-1)<<i));
+    }
+    public static void clearInRange(int number,int i,int j){
+        // 11100011 2,4 --> 11100000 | 00000011 --> -1 << j+1 | ~(-1<<i) 
+        int bitmask = (-1 << (j+1)) | (~(-1<<i));
+        System.out.println(number & bitmask);
+    }
     public static void main(String[] args) {
-        clearIthBit(16, 4);
+        // clearLastIthBit(15, 1);
+        clearInRange(15, 0, 3);
     }
 }
