aa50311726e2434592824c7f5979d358ee65667e
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-04 09:06:41 -0300

    Stack : using Liinked List

--- Stacks/Code.java
+++ Stacks/Code.java
@@ -3,29 +3,65 @@ import java.util.*;
 
 public class Code {
     static class Stack{
-        static ArrayList<Integer> list = new ArrayList<>();
-        public static boolean isEmpty(){
-            if(list.size()>0){
-                return false;
+        // static ArrayList<Integer> list = new ArrayList<>();
+        // public static boolean isEmpty(){
+        //     if(list.size()>0){
+        //         return false;
+        //     }
+        //     return true;
+        // }
+        // public static void push(int data){
+        //     list.add(data);
+        // }
+        // public static int pop(){
+        //     if(isEmpty()){
+        //         return Integer.MIN_VALUE;
+        //     }
+        //     int val = list.get(list.size()-1);
+        //     list.remove(list.size()-1);
+        //     return val;
+        // }
+        // public static void peek(){
+        //     if(isEmpty()){
+        //         return;
+        //     }
+        //     System.out.println(list.get(list.size()-1));
+        // }
+        public class Node{
+            int data;
+            Node next;
+            public Node(int data){
+                this.data=data;
+                this.next = null;
             }
-            return true;
         }
-        public static void push(int data){
-            list.add(data);
+        public static Node head=null;
+        public static Node tail=null;
+        public boolean isEmpty(){
+            return head==null;
         }
-        public static int pop(){
-            if(isEmpty()){
+        public void push(int data){
+            Node newNode = new Node(data);
+            if(head==null){
+                head=tail=newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+        public int pop(){
+            if(head==null){
                 return Integer.MIN_VALUE;
             }
-            int val = list.get(list.size()-1);
-            list.remove(list.size()-1);
+            int val = head.data;
+            head = head.next;
             return val;
         }
-        public static void peek(){
+        public void peek(){
             if(isEmpty()){
                 return;
             }
-            System.out.println(list.get(list.size()-1));
+            System.out.println(head.data);
         }
     }
     public static void main(String args[]){
