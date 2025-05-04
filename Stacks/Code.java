40a7666c32b7328bff730ece7e5b80639df06d09
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-04 09:11:10 -0300

    Stack : using Java Collection Framwork

--- Stacks/Code.java
+++ Stacks/Code.java
@@ -2,68 +2,68 @@ package Stacks;
 import java.util.*;
 
 public class Code {
-    static class Stack{
-        // static ArrayList<Integer> list = new ArrayList<>();
-        // public static boolean isEmpty(){
-        //     if(list.size()>0){
-        //         return false;
-        //     }
-        //     return true;
-        // }
-        // public static void push(int data){
-        //     list.add(data);
-        // }
-        // public static int pop(){
-        //     if(isEmpty()){
-        //         return Integer.MIN_VALUE;
-        //     }
-        //     int val = list.get(list.size()-1);
-        //     list.remove(list.size()-1);
-        //     return val;
-        // }
-        // public static void peek(){
-        //     if(isEmpty()){
-        //         return;
-        //     }
-        //     System.out.println(list.get(list.size()-1));
-        // }
-        public class Node{
-            int data;
-            Node next;
-            public Node(int data){
-                this.data=data;
-                this.next = null;
-            }
-        }
-        public static Node head=null;
-        public static Node tail=null;
-        public boolean isEmpty(){
-            return head==null;
-        }
-        public void push(int data){
-            Node newNode = new Node(data);
-            if(head==null){
-                head=tail=newNode;
-                return;
-            }
-            newNode.next = head;
-            head = newNode;
-        }
-        public int pop(){
-            if(head==null){
-                return Integer.MIN_VALUE;
-            }
-            int val = head.data;
-            head = head.next;
-            return val;
-        }
-        public void peek(){
-            if(isEmpty()){
-                return;
-            }
-            System.out.println(head.data);
-        }
-    }
+    // static class Stack{
+    //     // static ArrayList<Integer> list = new ArrayList<>();
+    //     // public static boolean isEmpty(){
+    //     //     if(list.size()>0){
+    //     //         return false;
+    //     //     }
+    //     //     return true;
+    //     // }
+    //     // public static void push(int data){
+    //     //     list.add(data);
+    //     // }
+    //     // public static int pop(){
+    //     //     if(isEmpty()){
+    //     //         return Integer.MIN_VALUE;
+    //     //     }
+    //     //     int val = list.get(list.size()-1);
+    //     //     list.remove(list.size()-1);
+    //     //     return val;
+    //     // }
+    //     // public static void peek(){
+    //     //     if(isEmpty()){
+    //     //         return;
+    //     //     }
+    //     //     System.out.println(list.get(list.size()-1));
+    //     // }
+    // //     public class Node{
+    // //         int data;
+    // //         Node next;
+    // //         public Node(int data){
+    // //             this.data=data;
+    // //             this.next = null;
+    // //         }
+    // //     }
+    // //     public static Node head=null;
+    // //     public static Node tail=null;
+    // //     public boolean isEmpty(){
+    // //         return head==null;
+    // //     }
+    // //     public void push(int data){
+    // //         Node newNode = new Node(data);
+    // //         if(head==null){
+    // //             head=tail=newNode;
+    // //             return;
+    // //         }
+    // //         newNode.next = head;
+    // //         head = newNode;
+    // //     }
+    // //     public int pop(){
+    // //         if(head==null){
+    // //             return Integer.MIN_VALUE;
+    // //         }
+    // //         int val = head.data;
+    // //         head = head.next;
+    // //         return val;
+    // //     }
+    // //     public void peek(){
+    // //         if(isEmpty()){
+    // //             return;
+    // //         }
+    // //         System.out.println(head.data);
+    // //     }
+    // }
     public static void main(String args[]){
         // Stack s = new Stack();
         // s.push(5);
@@ -73,12 +73,12 @@ public class Code {
         //     s.peek();
         //     s.pop();
         // }
-        Stack s = new Stack();
+        Stack<Integer> s = new Stack<>();
         s.push(4);
         s.push(3);
         s.push(2);
-        while(!s.isEmpty()){
-            s.peek();
+        while ((!s.isEmpty())) {
+            System.out.println(s.peek());
             s.pop();
         }
     }
