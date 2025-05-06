5d9855563c3284d8a3c87a298bf23e72446a67ff
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-05 23:10:16 -0300

    Recursion : Find last Occurance

--- Recursion/Code.java
+++ Recursion/Code.java
@@ -36,13 +36,24 @@ public class Code {
         }
         return findFirstOcc(arr, target, i+1);
     }
+    public static int findLastOcc(int arr[],int target,int i){
+        if(i==-1){
+            System.out.println("Target not found in array");
+            return -1;
+        }
+        if(arr[i]==target){
+            System.out.println("Index is : "+i);
+            return i;
+        }
+        return findFirstOcc(arr, target, i-1);
+    }
     public static void main(String[] args) {
         // printInDec(5);
         // printInInc(5);
         // System.out.println(findFact(5));
         // System.out.println(findSumOfN(5));
         int arr[] = {1,4,5,2,12,3,2};
-        findFirstOcc(arr, 2, 0);
+        findFirstOcc(arr, 2, arr.length-1);
 
     }
 }
