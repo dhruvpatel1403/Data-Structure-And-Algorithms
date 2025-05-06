0496bf1c2015f56c5cd5b11ccb3016adec2787a1
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-05 23:00:48 -0300

    Recursion : print in Increasing Order

--- Recursion/Code.java
+++ Recursion/Code.java
@@ -6,7 +6,15 @@ public class Code {
         System.out.println(n);
         printInDec(n-1);
     }
+    public static void printInInc(int n){
+        if(n==0){
+            return;
+        }
+        printInInc(n-1);
+        System.out.println(n);
+    }
     public static void main(String[] args) {
-        printInDec(5);
+        // printInDec(5);
+        printInInc(5);
     }
 }
