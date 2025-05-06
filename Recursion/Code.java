74dd9bef4918c0654d963ac963db5a26e9a82c2b
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-05 23:02:18 -0300

    Recursion : Find Factorial

--- Recursion/Code.java
+++ Recursion/Code.java
@@ -13,8 +13,15 @@ public class Code {
         printInInc(n-1);
         System.out.println(n);
     }
+    public static int findFact(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*findFact(n-1);
+    }
     public static void main(String[] args) {
         // printInDec(5);
-        printInInc(5);
+        // printInInc(5);
+        System.out.println(findFact(5));
     }
 }
