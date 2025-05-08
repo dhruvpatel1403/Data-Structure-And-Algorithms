e3080ef4dfd99184d3ee05ed66582f2f81068faf
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-07 23:20:48 -0300

    Queue : Intereleave two halves of a queue

--- QueueDS/Code2.java
+++ QueueDS/Code2.java
@@ -21,8 +21,35 @@ public class Code2 {
         }
         System.out.println();
     }
+    public static void interLeave(Queue<Integer> main){
+        int sz = main.size();
+        Queue<Integer> first = new LinkedList<>();
+        int i=0;
+        while(i<sz/2){
+            first.add(main.remove());
+            i++;
+        }
+        while(!first.isEmpty()){
+            main.add(first.remove());
+            main.add(main.remove());
+        }
+        System.out.println(main);
+    }
     public static void main(String args[]){
         String str = "aabccxb";
-        nonRepeating(str);
+        // nonRepeating(str);
+        Queue<Integer> q= new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        System.out.println(q);
+        interLeave(q);
     }
 }
