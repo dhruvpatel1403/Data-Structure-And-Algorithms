68835ed2c61153e49bc535cd061e564264846dcd
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-12 13:19:22 -0300

    Binary Tree : Post Order tree traversal

--- Tree/Code.java
+++ Tree/Code.java
@@ -44,17 +44,29 @@ public class Code {
             if(root == null){
                 return;
             }
-            
+
             inOrder(root.left);
             System.out.print(root.data+" ");
             inOrder(root.right);
         }
-    }
+        public static void postOrder(Node root){
+
+            if(root == null){
+                return;
+            }
+            
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data+" ");
+        }
+    }  
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinnaryTree tree = new BinnaryTree();
         Node root  = tree.buildeTree(nodes);
         System.out.println(root.data);
         tree.preOrder(root);
+        tree.inOrder(root);
+
     }
 }
