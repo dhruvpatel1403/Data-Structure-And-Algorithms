178e5e33474ca2b1f83ee2e3f0ca3512ae1dc287
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-12 13:12:08 -0300

    Binary tree : create a binary tree

---
+++ Tree/Code.java
@@ -0,0 +1,38 @@
+package Tree;
+public class Code {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+    static class BinnaryTree{
+        static int idx = -1;
+        public static Node buildeTree(int nodes[]){
+
+            idx++;
+
+            if(nodes[idx]==-1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+
+            newNode.left = buildeTree(nodes);
+            newNode.right = buildeTree(nodes);
+
+            return newNode;
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinnaryTree tree = new BinnaryTree();
+        Node root  = tree.buildeTree(nodes);
+        System.out.println(root.data);
+    }
+}
