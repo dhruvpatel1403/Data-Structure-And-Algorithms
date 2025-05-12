5ad1b39b16c3a47e0cf40b85a2d2d3c94d033260
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-12 13:17:37 -0300

    Binary Tree : In Order

--- Tree/Code.java
+++ Tree/Code.java
@@ -39,6 +39,16 @@ public class Code {
             preOrder(root.right);
 
         }
+        public static void inOrder(Node root){
+
+            if(root == null){
+                return;
+            }
+            
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
