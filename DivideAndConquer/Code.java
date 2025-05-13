2abcd026d0d745ba86c9e1ef6a8cb2c72545f756
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-13 09:52:28 -0300

    Devide and Conquere : Quick Sort

--- DivideAndConquer/Code.java
+++ DivideAndConquer/Code.java
@@ -28,9 +28,36 @@ public class Code {
             }
         }
     }
+    public static void quickSort(int arr[],int st,int end){
+        if(st>=end){
+            return;
+        }
+        int pivot = partition(arr,st,end);
+        quickSort(arr, st, pivot-1);
+        quickSort(arr, pivot+1, end);
+    }
+    public static int partition(int arr[],int st,int end){
+        int i = st-1;
+        int pivot = arr[end];
+        for(int j=st;j<end;j++){
+            if(arr[j]<=pivot){
+                int temp = arr[j];
+                arr[j] = arr[++i];
+                arr[i] = temp;
+            }
+        }
+        int temp = arr[end];
+        arr[end] = arr[++i];
+        arr[i] = temp;
+        return i;
+    }
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
-        serchInRotated(arr, 0, arr.length-1, target);
+        // int target = 0;
+        // serchInRotated(arr, 0, arr.length-1, target);
+        quickSort(arr, 0, arr.length-1);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
     }
 }
