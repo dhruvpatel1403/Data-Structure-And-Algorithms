2cdc5b649a1d08b54f66fe9be9aa569787d070cc
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-17 09:43:58 -0300

    Binary Tree : In order and Level order traversal

--- Tree/Code2.java
+++ Tree/Code2.java
@@ -25,7 +25,54 @@ public class Code2 {
             return newNode;
         }
         
-        
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data+ " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+        public static void inOrder(Node root){
+            if(root == null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
+        public static void postOrder(Node root){
+            if(root == null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data+" ");
+        }
+        public static void levelOrder(Node root){
+            Queue<Node> q= new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty()){
+                Node curr = q.remove();
+                if(curr == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(curr.data+ " ");
+                    if(curr.left != null){
+                        q.add(curr.left);
+                    }
+                    if(curr.right != null){
+                        q.add(curr.right);
+                    }
+                }
+            }
+        }
     }
     
     public static void main(String[] args) {
@@ -33,12 +80,12 @@ public class Code2 {
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
-        // tree.preOrder(root);
-        // System.out.println();
-        // tree.inOrder(root);
-        // System.out.println();
-        // tree.postOrder(root);
-        // System.out.println();
-        // tree.levelOrder(root);
+        tree.preOrder(root);
+        System.out.println();
+        tree.inOrder(root);
+        System.out.println();
+        tree.postOrder(root);
+        System.out.println();
+        tree.levelOrder(root);
     }
 }
