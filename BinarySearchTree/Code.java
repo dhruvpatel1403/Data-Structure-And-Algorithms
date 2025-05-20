9d17532f9d24072dcab6fa8e7571f363bf23e374
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-20 17:12:25 -0300

    BST : Delete a Node using in order successor

--- BinarySearchTree/Code.java
+++ BinarySearchTree/Code.java
@@ -46,14 +46,52 @@ public class Code {
                 return search(root.right, key);
             }
         }
+        public static Node InorderSuccessor(Node root){
+            while(root.left != null){
+                root = root.left;
+            }
+            return root;
+        }
+        public static Node delete(Node root,int val){
+            if(root == null){
+                return null;
+            }
+            if(root.data > val){
+                root.left =  delete(root.left, val);
+            }else if(root.data < val){
+                root.right = delete(root.right, val);
+            }
+            else{
+                //case 1
+                if(root.left == null && root.right == null){
+                    return null;
+                }
+                //case 2
+                else if(root.left == null){
+                    return root.right;
+                }
+                else if(root.right == null){
+                    return root.left;
+                }
+                //case 3
+                else{
+                    Node IS = InorderSuccessor(root.right);
+                    root.data = IS.data;
+                    root.right =  delete(root.right, IS.data);
+                }
+            }
+            return root;
+        }
     public static void main(String[] args) {
         int arr[] = {5,1,3,4,2,7};
         Node root= null;
         for(int i=0;i<arr.length;i++){
             root = buildTree(arr[i],root);
         }
-        // inOrder(root);
-        System.out.println(search(root, 10));
+        inOrder(root);
+        System.out.println();
+        delete(root, 3);
+        inOrder(root);
     }
     
 }
