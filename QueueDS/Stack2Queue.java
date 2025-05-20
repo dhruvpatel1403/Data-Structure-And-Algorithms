41e06595be8a02f8536cfc6739a312c3be69e072
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-20 09:11:39 -0300

    Queue : Queue using two stack optimized

--- QueueDS/Stack2Queue.java
+++ QueueDS/Stack2Queue.java
@@ -1,43 +1,129 @@
+// package QueueDS;
+// import java.util.*;
+
+// public class Stack2Queue {
+//     public static class Queue{
+//         static Stack<Integer> first = new Stack<>();
+//         static Stack<Integer> second = new Stack<>();
+//         public static boolean isEmpty(){
+//             return first.isEmpty();
+//         }
+//         public static void add(int data){
+//             // while(!first.isEmpty()){
+//             //     second.push(first.pop());
+//             // }
+//             // first.push(data);
+//             // while(!second.isEmpty()){
+//             //     first.push(second.pop());
+//             // }
+//             first.push(data);
+//         }
+//         public static int remove(){
+//             if(first.isEmpty()){
+//                 System.out.println("Queue is already empty");
+//                 return -1;
+//             }
+//             while(!first.isEmpty()){
+//                 second.push(first.pop());
+//             }
+//             int val = second.pop();
+//             while(!second.isEmpty()){
+//                 first.push(second.pop());
+//             }
+//             return val;
+//             // return first.pop();
+//         }
+//         public static int peek(){
+//             if(isEmpty()){
+//                 System.out.println("Queue is already empty");
+//                 return -1;
+//             }
+//             while (!first.isEmpty()) {
+//                 second.push(first.pop());
+//             }
+//             int val = second.peek();
+//             while(!second.isEmpty()){
+//                 first.push(second.pop());
+//             }
+//             return val;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Queue q = new Queue();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         while(!q.isEmpty()){
+//             System.out.println(q.peek());
+//             q.remove();
+//         }   
+//     }
+// }
 package QueueDS;
 import java.util.*;
 
 public class Stack2Queue {
-    public static class Queue{
-        static Stack<Integer> first = new Stack<>();
-        static Stack<Integer> second = new Stack<>();
-        public static boolean isEmpty(){
-            return first.isEmpty();
+    public static class OptimizedQueue {
+        static Stack<Integer> inputStack = new Stack<>();
+        static Stack<Integer> outputStack = new Stack<>();
+        
+        public static boolean isEmpty() {
+            return inputStack.isEmpty() && outputStack.isEmpty();
         }
-        public static void add(int data){
-            while(!first.isEmpty()){
-                second.push(first.pop());
-            }
-            first.push(data);
-            while(!second.isEmpty()){
-                first.push(second.pop());
-            }
+        
+        // O(1) time
+        public static void add(int data) {
+            inputStack.push(data);
         }
-        public static int remove(){
-            if(isEmpty()){
+        
+        // Amortized O(1) time
+        public static int remove() {
+            if (isEmpty()) {
                 System.out.println("Queue is already empty");
                 return -1;
             }
-            return first.pop();
+            
+            // Only transfer if output stack is empty
+            if (outputStack.isEmpty()) {
+                while (!inputStack.isEmpty()) {
+                    outputStack.push(inputStack.pop());
+                }
+            }
+            
+            return outputStack.pop();
         }
-        public static int peek(){
-            if(isEmpty()){
+        
+        // Amortized O(1) time
+        public static int peek() {
+            if (isEmpty()) {
                 System.out.println("Queue is already empty");
                 return -1;
             }
-            return first.peek();
+            
+            // Only transfer if output stack is empty
+            if (outputStack.isEmpty()) {
+                while (!inputStack.isEmpty()) {
+                    outputStack.push(inputStack.pop());
+                }
+            }
+            
+            return outputStack.peek();
         }
     }
+    
     public static void main(String[] args) {
-        Queue q = new Queue();
+        OptimizedQueue q = new OptimizedQueue();
         q.add(1);
         q.add(2);
         q.add(3);
-        while(!q.isEmpty()){
+        q.remove();
+        q.add(3);
+        q.remove();
+        q.remove();
+        q.remove();
+        q.add(2);
+        q.add(4);
+        while (!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
         }   
