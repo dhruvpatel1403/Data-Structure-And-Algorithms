3af39c452ea674d9caa0b7336bf24be89b925642
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-25 22:44:51 -0300

    Heap : using classes as data type in priority queue

--- Heap/Code.java
+++ Heap/Code.java
@@ -1,14 +1,26 @@
 package Heap;
 import java.util.*;
 public class Code {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name,int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(3);
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("Dhruv",1));
+        pq.add(new Student("Dhruvansh",5));
+        pq.add(new Student("Alay",10));
+        pq.add(new Student("Jay",2));
         while(!pq.isEmpty()){
-            System.out.println(pq.peek());
+            System.out.println(pq.peek().name);
             pq.remove();
         }
     }
