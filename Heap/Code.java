29b2ff372da3800eae5c0f4de00e1ebf2ab66247
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-26 14:05:37 -0300

    Heap : Inserting inside heap

--- Heap/Code.java
+++ Heap/Code.java
@@ -1,5 +1,7 @@
 package Heap;
 import java.util.*;
+
+import sorting.sorting;
 public class Code {
     static class Student implements Comparable<Student>{
         String name;
@@ -13,6 +15,19 @@ public class Code {
             return this.rank - s2.rank;
         }
     }
+    static class Heap{
+        ArrayList<Integer> h = new ArrayList<>();
+        public void insert(int data){
+            h.add(data);
+            int x= h.size() -1;
+            int par = ( h.size() - 2 ) / 2;
+            while(h.get(x) < h.get(par)){
+                int temp = h.get(x);
+                h.set(x, h.get(par));
+                h.set(par, temp);
+            }
+        }
+    }
     public static void main(String[] args) {
         PriorityQueue<Student> pq = new PriorityQueue<>();
         pq.add(new Student("Dhruv",1));
@@ -23,5 +38,11 @@ public class Code {
             System.out.println(pq.peek().name);
             pq.remove();
         }
+        Heap h = new Heap();
+        h.insert(3);
+        h.insert(4);
+        h.insert(2);
+        h.insert(1);
+        System.out.println(h.h);
     }
 }
