d5a57f8dc97f1111d038fde8e9de3e7274e0af04
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-25 22:31:35 -0300

    Heap : Using JCF priority Queue

---
+++ Heap/Code.java
@@ -0,0 +1,15 @@
+package Heap;
+import java.util.*;
+public class Code {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+    }
+}
