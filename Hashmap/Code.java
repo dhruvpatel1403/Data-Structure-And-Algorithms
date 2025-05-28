7d938f299c291dca54abebf4c3bebeaadd7d8876
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-28 19:01:44 -0300

    Hashmap : total subarray with sum equal to k

--- Hashmap/Code.java
+++ Hashmap/Code.java
@@ -1,8 +1,7 @@
 package Hashmap;
 import java.util.*;
 public class Code {
-    public static void main(String[] args) {
-        int arr[] = { 15, -2, 2, -8, 1, 7, 10, 23};
+    public static void sumZero(int arr[]){
         HashMap<Integer, Integer> hm = new HashMap<>();
         int sum = 0;
         int len = 0;
@@ -17,5 +16,25 @@ public class Code {
         }
         System.out.println(len);
     }
-   
+    public static void sumK(int arr[],int k){
+        int sum = 0;
+        int ans = 0;
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0,1);
+
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+            if(hm.containsKey(sum-k)){
+                ans += hm.get(sum-k);
+            }
+            hm.put(sum, hm.getOrDefault(sum, 0)+1);
+        }
+        System.out.println(ans);
+    }
+    public static void main(String[] args) {
+        int arr[] = { 10,2,-2,-20,10};
+        sumZero(arr);
+        sumK(arr, -10);
+    }
 }
