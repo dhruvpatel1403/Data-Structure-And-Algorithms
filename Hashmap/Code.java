ab3d1791b5f09ff3bba34b9b70fa13717a59b712
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-28 18:26:00 -0300

    Hashmap : largest subarry sum to 0

---
+++ Hashmap/Code.java
@@ -0,0 +1,21 @@
+package Hashmap;
+import java.util.*;
+public class Code {
+    public static void main(String[] args) {
+        int arr[] = { 15, -2, 2, -8, 1, 7, 10, 23};
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+        for(int i=0;i<arr.length;i++){
+            sum+= arr[i];
+            if(hm.containsKey(sum)){
+                len = Math.max(len,i - hm.get(sum));
+            }
+            else{
+                hm.put(sum, i);
+            }
+        }
+        System.out.println(len);
+    }
+   
+}
