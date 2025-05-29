e257ce45dfda2dacdcce621e94fddb2e4e23e45d
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-29 08:46:14 -0300

    Priority Queue : Using object in priority Queue

--- Heap/Code2.java
+++ Heap/Code2.java
@@ -1,15 +1,27 @@
 package Heap;
 import java.util.*;
 public class Code2 {
+    public static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name,int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(4);
-        pq.add(1);
-        pq.add(3);
-        pq.add(7);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("Dhruv",1));
+        pq.add(new Student("Dhruvansh",100));
+        pq.add(new Student("Alay",100000));
+        pq.add(new Student("Jax",2));
         // System.out.println(pq);
         while(!pq.isEmpty()){
-            System.out.println(pq.remove());
+            System.out.println(pq.remove().rank);
         }
     }
 }
