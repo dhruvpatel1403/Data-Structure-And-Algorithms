8f683ec551f75146b6c2d1946dc0cb37efb9b8eb
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-06-05 08:44:17 -0300

    Graph : BFS traversal

---
+++ Graph/Code2.java
@@ -0,0 +1,64 @@
+package Graph;
+import java.util.*;
+public class Code2 {
+    static class Edge{
+        int src;
+        int dst;
+        int wt;
+        public Edge(int s,int d,int w){
+            this.src = s;
+            this.dst = d;
+            this.wt = w;
+        }
+    }
+    public static void BFS(ArrayList<Edge>[] graph){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[graph.length];
+        q.add(0);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if(!vis[curr]){
+                System.out.print(curr+" ");
+                vis[curr] = true;
+                for(int i=0;i<graph[curr].size();i++){
+                    q.add(graph[curr].get(i).dst);
+                }
+            }
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        int v = 5;
+        ArrayList<Edge>[] graph = new ArrayList[v]; // null convert it to new ArrayList
+
+        for(int i=0;i<v;i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0 
+        graph[0].add(new Edge(0,1,5));
+
+        // 1
+        graph[1].add(new Edge(1,0,5));
+        graph[1].add(new Edge(1,2,1));
+        graph[1].add(new Edge(1,3,3));
+
+
+        // 2
+        graph[2].add(new Edge(2,1,1));
+        graph[2].add(new Edge(2,3,1));
+        graph[2].add(new Edge(2,4,2));
+
+
+        // 3
+
+        graph[3].add(new Edge(3,2,1));
+        graph[3].add(new Edge(3,1,3));
+
+
+        // 4
+        graph[4].add(new Edge(2,4,2));
+        BFS(graph);
+    }
+}
