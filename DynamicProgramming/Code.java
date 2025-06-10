ac6504f225e12c4b035892c352f0581c4d3a6732
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-06-10 16:25:39 -0300

    DP : Fibbonacci using tabulation approach

--- DynamicProgramming/Code.java
+++ DynamicProgramming/Code.java
@@ -10,9 +10,19 @@ public class Code {
         f[n] = fib(n-1, f) + fib(n-2, f);
         return f[n];
     }
+    public static int fibTabulation(int n){
+        int fib[] = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i=2;i<=n;i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[n];
+    }
     public static void main(String[] args) {
         int n= 10;
         int f[] = new int[n+1];
-        System.out.println(fib(n,f));   
+        System.out.println(fib(n,f));
+        System.out.println(fibTabulation(10));  
     }
 }
