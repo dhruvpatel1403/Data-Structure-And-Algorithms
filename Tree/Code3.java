ff72478a77016cce588c82e06e44d07d445c370d
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-06-16 20:23:57 -0300

    Binary Tree : Count Nodes of BT

--- Tree/Code3.java
+++ Tree/Code3.java
@@ -69,6 +69,14 @@ public class Code3 {
         int rh = height(root.right);
         return Math.max(lh,rh) + 1;
     }
+    public static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lc = countNodes(root.left);
+        int rc = countNodes(root.right);
+        return lc + rc + 1;
+    }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root = buildTree(nodes);
@@ -76,5 +84,6 @@ public class Code3 {
         System.out.println();
         levelOrder(root);
         System.out.println(height(root));
+        System.out.println(countNodes(root));
     }
 }
