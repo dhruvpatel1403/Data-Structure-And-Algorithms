d28ccbba7e6677c2183db0bd838451e0603057f4
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-06-26 23:01:22 -0300

    Graph: detect cycle in directed graph

--- Graph/Code3.java
+++ Graph/Code3.java
@@ -71,46 +71,81 @@ public class Code3 {
                 return true;
             }
             if(!vis[graph[curr].get(i).dst]){
-                return detect_cycle_util(graph, vis, graph[curr].get(i).dst, curr);
+                if(detect_cycle_util(graph, vis, graph[curr].get(i).dst, curr)){
+                    return true;
+                }
             }
         }
         return false;
     }
+    public static boolean detect_cycle_DFS(ArrayList<Edge>[] graph){
+      boolean vis[] = new boolean[graph.length];
+      boolean stack[] = new boolean[graph.length];
+      for(int i=0;i<graph.length;i++){
+        if(!vis[i]){
+            if(detect_cycle_dfs_util(graph,i,vis,stack)){
+                return true;
+            }
+        }
+      }
+      return false;
+    }
+    public static boolean detect_cycle_dfs_util(ArrayList<Edge>[] graph,int curr,boolean vis[],boolean stack[]){
+        vis[curr] = true;
+        stack[curr] = true;
+        for(int i=0;i<graph[curr].size();i++){
+            if(stack[graph[curr].get(i).dst]){
+                return true;
+            }
+            if(!vis[graph[curr].get(i).dst]){
+                if(detect_cycle_dfs_util(graph, graph[curr].get(i).dst, vis, stack)){
+                    return true;
+                }
+            }
+        }
+        stack[curr] = false;
+        return false;
+    }
     public static void main(String[] args) {
-        int v = 5;
+        int v = 4;
         ArrayList<Edge>[] graph = new ArrayList[v];
         for(int i=0;i<v;i++){
             graph[i] = new ArrayList<>();
         }
          // 0 
-        graph[0].add(new Edge(0,1,5));
+        // graph[0].add(new Edge(0,1,5));
 
-        // 1
-        graph[1].add(new Edge(1,0,5));
-        graph[1].add(new Edge(1,2,1));
-        graph[1].add(new Edge(1,3,3));
-
-
-        // 2
-        graph[2].add(new Edge(2,1,1));
-        graph[2].add(new Edge(2,3,1));
-        graph[2].add(new Edge(2,4,2));
+        // // 1
+        // graph[1].add(new Edge(1,0,5));
+        // graph[1].add(new Edge(1,2,1));
+        // graph[1].add(new Edge(1,3,3));
 
 
-        // 3
-        graph[3].add(new Edge(3,2,1));
-        graph[3].add(new Edge(3,1,3));
+        // // 2
+        // graph[2].add(new Edge(2,1,1));
+        // graph[2].add(new Edge(2,3,1));
+        // graph[2].add(new Edge(2,4,2));
 
 
-        // 4
-        graph[4].add(new Edge(2,4,2));
+        // // 3
+        // graph[3].add(new Edge(3,2,1));
+        // graph[3].add(new Edge(3,1,3));
+
+
+        // // 4
+        // graph[4].add(new Edge(2,4,2));
+        graph[0].add(new Edge(0, 1,1));
+        graph[1].add(new Edge(1, 2,2));
+        graph[2].add(new Edge(2, 0,3));
         // for(int i=0;i<graph[1].size();i++){
         //     System.out.print(graph[1].get(i).dst+" ");
         // }
         BFS(graph);
         System.out.println();
         DFS(graph, new boolean[v], 0);
-        System.out.println(hashPath(graph, new boolean[v], 0, 4));
-        detect_cycle(graph);
+        // System.out.println(hashPath(graph, new boolean[v], 0, 4));
+        // detect_cycle(graph);
+        // detect_cycle_DFS(graph);
+        System.out.println(detect_cycle(graph));
     }
 }
