d3eca6e95ec3b4af61655a23c31395b149756a3a
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-06-29 12:47:03 -0300

    DP: countways using tabulation method

--- DynamicProgramming/Code2.java
+++ DynamicProgramming/Code2.java
@@ -17,7 +17,19 @@ public class Code2 {
 
         return ways[n];
     }
-    
+    public static void countWaysTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        for(int i=1;i<=n;i++){
+            if(i== 1){
+                dp[i] = dp[i-1];
+            }
+            else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        System.out.println(dp[n]);
+     }
     public static void main(String[] args) {
         int n=5;
         int ways[] = new int[n+1];
