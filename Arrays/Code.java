6bd60995aa55dbff8afb8b9eb9c66dd5be4edce3
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-03 12:40:17 -0300

    Binary Search: peak index in mountain array

--- Arrays/Code.java
+++ Arrays/Code.java
@@ -144,6 +144,23 @@ public class Code{
         }
         System.out.println(maxProfit);
     }
+    public int peakIndexInMountainArray(int[] arr) {
+        int st = 1;
+        int end = arr.length - 2;
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            if(arr[mid]> arr[mid-1] && arr[mid]> arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid-1]){
+                st = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
     public static void main(String args[]){
         int arr[] = {7,1,5,3,6,4};
         // search(arr, 0, 0);
