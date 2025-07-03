12a7ab072690769f14ad60588258e79a1df9ed85
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-03 12:41:06 -0300

    Binary search: single element in sorted array

--- Arrays/Code.java
+++ Arrays/Code.java
@@ -161,6 +161,44 @@ public class Code{
         }
         return -1;
     }
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        int st = 0;
+        int arr[] = nums;
+        int end = nums.length - 1;
+        while(st <= end){
+            int mid = st + (end -st)/2;
+            if(mid == 0 && nums[0] != nums[1]){
+                return nums[0];
+            }
+            if(mid == nums.length-1 && nums[mid] != nums[mid-1]){
+                return nums[mid];
+            }
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            if(mid % 2 == 0){
+                if(nums[mid-1]==nums[mid]){
+                    end = mid-1;
+                }
+                else{
+                    st = mid+1;
+                }
+            }
+            else{
+                if(nums[mid-1]==nums[mid]){
+                    st = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+
+        return -1;
+    }
     public static void main(String args[]){
         int arr[] = {7,1,5,3,6,4};
         // search(arr, 0, 0);
