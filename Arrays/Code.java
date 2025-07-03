86d355f4e5edb1944f7ffbf825e19fe9a30dd53e
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-03 12:42:11 -0300

    Binary Search: search in sorted rotated array

--- Arrays/Code.java
+++ Arrays/Code.java
@@ -199,6 +199,34 @@ public class Code{
 
         return -1;
     }
+    public int search(int[] nums, int target) {
+        int st = 0;
+        int end = nums.length-1;
+        
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[st]){
+                if(nums[st]<=target && target<=nums[mid]){
+                    end = mid-1;
+                }
+                else{
+                    st = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[end]){
+                    st=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
     public static void main(String args[]){
         int arr[] = {7,1,5,3,6,4};
         // search(arr, 0, 0);
