d1436c4f185ae6aba4558aee62436184684662a1
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-06 11:58:04 -0300

    Recursion: Print in incr

--- Recursion/Code3.java
+++ Recursion/Code3.java
@@ -7,7 +7,16 @@ public class Code3 {
         System.out.print(n+" ");
         printDec(n-1);
     }
+    public static void printInc(int n){
+        if(n==0){
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String[] args) {
         printDec(5);
+        System.out.println();
+        printInc(5);
     }
 }
