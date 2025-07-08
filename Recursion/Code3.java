72b9ccff7c96c59dca61eb9a5f94528b3405703f
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-07 21:58:43 -0300

    Recursion: NQueens Problem

--- Recursion/Code3.java
+++ Recursion/Code3.java
@@ -86,19 +86,91 @@ public class Code3 {
             printsubSet(str, ans+str.charAt(ind), ind+1);
             printsubSet(str, ans, ind+1);
         }
+//         public static void permutation(String str, String ans) {
+//     if (ans.length() == str.length()) {
+//         System.out.println(ans);
+//         return;
+//     }
+
+//     HashSet<Character> used = new HashSet<>();
+
+//     for (int i = 0; i < str.length(); i++) {
+//         char curr = str.charAt(i);
+//         if (used.contains(curr)) continue; // Skip duplicates
+//         used.add(curr);
+
+//         String newStr = str.substring(0, i) + str.substring(i + 1);
+//         permutation(newStr, ans + curr);
+//     }
+// }
+    public static void nQueens(int n,char arr[][],int row){
+        if(row == arr.length){
+            printArr(arr);
+            System.out.println();
+            count++;
+            return;
+        }
+        for(int col=0;col<arr.length;col++){
+            if(isSafe(arr,row,col)){
+                arr[row][col] = 'Q';
+                nQueens(n, arr, row+1);
+                arr[row][col] = 'X';
+            }
+        }
+    }
+    public static void printArr(char arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j] != 'Q'){
+                    System.out.print("X ");
+                }else{
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static boolean isSafe(char arr[][],int row,int col){
+        for(int i=0;i<row;i++){
+            if(arr[i][col]=='Q'){
+                return false;
+            }
+        }
+        for(int j=0;j<col;j++){
+            if(arr[row][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1;i>=0&& j>=0; i--,j--){
+            if(arr[i][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<arr.length;i--,j++){
+            if(arr[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    static int count=0;
     public static void main(String[] args) {
-        printDec(5);
-        System.out.println();
-        printInc(5);
-        System.out.println();
-        System.out.println(findFact(5));
-        System.out.println(sumOfN(5));
-        int arr[] = { 5,5,5,5};
-        System.out.println(findFirstOcc(arr, 0, 5));
-        System.out.println(lastOccu(arr, 0, 5));
-        remveDup("appnacollege", 0, new boolean[26], new StringBuilder(""));
-        System.out.println(frdPair(3));
-        printBSt(3, 0, "");
-        printsubSet("abc", "", 0);
+        // printDec(5);
+        // System.out.println();
+        // printInc(5);
+        // System.out.println();
+        // System.out.println(findFact(5));
+        // System.out.println(sumOfN(5));
+        // int arr[] = { 5,5,5,5};
+        // System.out.println(findFirstOcc(arr, 0, 5));
+        // System.out.println(lastOccu(arr, 0, 5));
+        // remveDup("appnacollege", 0, new boolean[26], new StringBuilder(""));
+        // System.out.println(frdPair(3));
+        // printBSt(3, 0, "");
+        // printsubSet("abc", "", 0);
+        // permutation("abcd", "");
+        int n=7;
+        nQueens(n, new char[n][n], 0);
+        System.out.println(count);
     }
 }
