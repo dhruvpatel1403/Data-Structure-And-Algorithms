0c9c2d5d567396007429c580ab2c50e81d0865bd
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-10 10:16:11 -0300

    LinkedList: add last

--- LinkedList/Code.java
+++ LinkedList/Code.java
@@ -15,7 +15,7 @@ public class Code {
         Node newNode = new Node(data);
 
         if(head == null){
-            head = newNode;
+            head = tail =  newNode;
             return;
         }
 
@@ -25,15 +25,27 @@ public class Code {
     public void printLinkedList(){
         Node temp = head;
         while(temp != null){
-            System.out.print(temp.data+"-> ");
+            System.out.print(temp.data+" --> ");
             temp = temp.next;
 
         }
+        System.out.println("null");
     }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    
     public static void main(String[] args) {
         Code ll = new Code();
         ll.addFirst(10);
         ll.addFirst(15);
-        System.out.println("hii");
+        ll.addLast(20);
+        ll.printLinkedList();
     }
 }
