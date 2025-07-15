2b291d43897d118466fa897cacfaae46ea67f919
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-15 17:06:11 -0300

    Binary Tree: Level order traversal

--- Tree/Code4.java
+++ Tree/Code4.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import java.util.*;
+
 public class Code4 {
     static class Node{
         int data;
@@ -38,11 +40,39 @@ public class Code4 {
         System.out.print(root.data+ " ");
         inOrder(root.right);
     }
+
+    public static void levelOrder(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node curr  = q.remove();
+            if(curr == null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }else{
+                System.out.print(curr.data+" ");
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root = buildetree(arr);
         preorder(root);
         System.out.println();
         inOrder(root);
+        System.out.println();
+        levelOrder(root);
     }
 }
