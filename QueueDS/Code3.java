5916331fd2a28314187ea9f8648a0441eaa0b096
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-15 17:00:39 -0300

    Binary Tree: builde tree

---
+++ QueueDS/Code3.java
@@ -0,0 +1,15 @@
+package QueueDS;
+import java.util.*;
+
+public class Code3 {
+    public static void nonRep(String str){
+        boolean freq[] = new boolean[26];
+        Queue<Character> q = new LinkedList<>();
+        for(int i=0;i<str.length();i++){
+             
+        }
+    }
+    public static void main(String args[]){
+
+    }
+}
