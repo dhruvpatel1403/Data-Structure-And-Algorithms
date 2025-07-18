c86cbc09371d88c132a3d30cff06d09571224df5
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-07-18 20:03:01 -0300

    Binary Tree: convert to sum tree

--- Tree/Code4.java
+++ Tree/Code4.java
@@ -220,6 +220,18 @@ public class Code4 {
         }
         return d;
     }
+    public static int sumTree(Node root){
+        if(root == null){
+            return 0;
+        }
+        int data = root.data;
+        int left = sumTree(root.left);
+        int right = sumTree(root.right);
+        int leftN = (root.left == null) ? 0 : root.left.data;
+        int rightN = (root.right == null) ? 0 : root.right.data;
+        root.data = left + leftN + right + rightN;
+        return data; 
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root = buildetree(arr);
@@ -239,6 +251,8 @@ public class Code4 {
         // topView(root);
         // System.out.println(lca(root, 3, 5).data);
         // System.out.println(minDist(root, 3, 5));
-        kthAns(root, 5, 2);
+        // kthAns(root, 5, 2);
+        sumTree(root);
+        preorder(root);
     }
 }
